a9b051f2a9b8a70ef67b46d6f3c8fa6fb94ef01e
sbdobrescu <sbdobrescu@gmail.com>
2015-10-16 15:51:42 -0400

    Update ThermostatDir.groovy

--- ThermostatDir.groovy
+++ ThermostatDir.groovy
@@ -19,7 +19,7 @@ definition(
 	name: "Enhanced Thermostat Director",
 	namespace: "tslagle13",
 	author: "Tim Slagle",
-	description: "Changes mode of your thermostat based on the temperature range of a specified temperature sensor and shuts off the thermostat if any windows/doors are open.",
+	description: "Changes settings of your thermostat based on the temperature range of a specified temperature sensor and shuts off the thermostat if any windows/doors are open.",
 	category: "Green Living",
 	iconUrl: "http://icons.iconarchive.com/icons/icons8/windows-8/512/Science-Temperature-icon.png",
 	iconX2Url: "http://icons.iconarchive.com/icons/icons8/windows-8/512/Science-Temperature-icon.png"
