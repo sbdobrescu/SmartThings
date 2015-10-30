0128e5ad31fe57c30a03779e0040ac35447753a1
sbdobrescu <sbdobrescu@gmail.com>
2015-10-30 11:18:23 -0400

    Run a routine when switch turns on

---
+++ RoutineSwitch.groovy
@@ -0,0 +1,58 @@
+/**
+ *  Activate A Routine When Switch Turns On
+ *
+ *  Copyright 2015 Bobby Dobrescu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Ties a Hello, Home phrase to a switch's (virtual or real) on/off state. Perfect for use with IFTTT.
+ *  Simple define a switch to be used, then tie the on/off state of the switch to a specific Hello, Home phrases.
+ *  Connect the switch to an IFTTT action, and the Hello, Home phrase will fire with the switch state change.
+ *
+ *
+ */
+definition(
+    name: "Activate a Routine when Switch Turns On",
+    namespace: "sbdobrescu",
+    author: "Bobby Dobrescu",
+    description: "Fire a routine everytime a switch turns on.",
+    category: "Convenience",
+    iconUrl: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/IFTTT-SmartApps/App1.png",
+    iconX2Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/IFTTT-SmartApps/App1@2x.png",
+    iconX3Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/IFTTT-SmartApps/App1@2x.png")
+
+
+preferences {
+
+    section("Choose a switch to use...") {
+		input "controlSwitch", "capability.switch", title: "Switch", multiple: false, required: true
+    }
+    section("Perform the following phrase when this switch turns on...") {
+				log.trace phrases
+				input "phrase_on", "enum", title: "Select switch", required: true, options: phrases
+			}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	subscribe(controlSwitch, "switch", "switchHandler")
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+	unsubscribe()
+	subscribe(controlSwitch, "switch", "switchHandler")
+}
+
+def switchHandler(evt) {
+	if (evt.value == "on") {
+    	location.helloHome.execute(settings.phrase_on)
+	}
+}
